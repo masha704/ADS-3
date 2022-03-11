16cd9b1eee43e7f57a6f85e20e93e1d60ac3f24c
Maria Anikina <59134557+masha704@users.noreply.github.com>
2022-03-11 17:09:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,23 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int i = 0;
+    int j = size - 1;
+    int counter = 0;
+    while (i < j) {
+        if (arr[i] == value) {
+            counter++;
+        }
+        if (arr[j] == value) {
+            counter++;
+        }
+        i++;
+        j--;
+    }
+    if (i == j) {
+        if (arr[i] == value) {
+            counter++;
+        }
+    }
+    return counter;
 }
